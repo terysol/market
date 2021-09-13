6c236e1a383f3440ba251c41f41a5b0ee9f7282a
solmin <solminkim03@naver.com>
2021-09-14 00:05:26 +0900

    09.14 - review

--- js/jquery.js
+++ js/jquery.js
@@ -40,63 +40,70 @@
 
 $(document).ready(function() {
     var xhr = new XMLHttpRequest();
-    console.log(xhr.readyState);
+    console.log("callback");
     xhr.onreadystatechange = function() { // 요청에 대한 콜백
     if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
+        console.log("status",xhr.status);
         if (xhr.status === 200 || xhr.status === 201) {
+            console.log("status",xhr.status);
+            console.log("200");
+            console.log(xhr.responseText);
+            $("#bestProducts").empty();
+            $("#bestProducts").append($(xhr.responseText));
             console.log(xhr.responseText);
         } else {
             console.error(xhr.responseText);
         }
     }
     };
+    
     xhr.open('GET', '/api/proudcts/best'); // 메소드와 주소 설정
     xhr.send();
 
-    const products = [
-        {
-            name: "[키친아트]키친아트 후라이팬 1+1",
-            thumbnail: "product1.jpeg",
-            price: 14900
-        },
-        {
-            name: "[베베숲]아기물티슈 프리미어 캡70매 20팩",
-            thumbnail: "product2.jpeg",
-            price: 27900
-        },
-        {
-            name: "[웨스트우드]웨스트우드 가을오픈 자켓/티셔츠/팬츠 세일전",
-            thumbnail: "product3.jpeg",
-            price: 19000
-        },
-        {
-            name: "[리챔]리챔 오리지널 200g 10캔",
-            thumbnail: "product4.jpeg",
-            price: 18900
-        },
-        {
-            name: "[특등급] 당진해나루쌀 삼광미 10kg 2포 / 총20kg",
-            thumbnail: "product6.jpeg",
-            price: 67900
-        }
-    ];
+    // const products = [
+    //     {
+    //         name: "[키친아트]키친아트 후라이팬 1+1",
+    //         thumbnail: "product1.jpeg",
+    //         price: 14900
+    //     },
+    //     {
+    //         name: "[베베숲]아기물티슈 프리미어 캡70매 20팩",
+    //         thumbnail: "product2.jpeg",
+    //         price: 27900
+    //     },
+    //     {
+    //         name: "[웨스트우드]웨스트우드 가을오픈 자켓/티셔츠/팬츠 세일전",
+    //         thumbnail: "product3.jpeg",
+    //         price: 19000
+    //     },
+    //     {
+    //         name: "[리챔]리챔 오리지널 200g 10캔",
+    //         thumbnail: "product4.jpeg",
+    //         price: 18900
+    //     },
+    //     {
+    //         name: "[특등급] 당진해나루쌀 삼광미 10kg 2포 / 총20kg",
+    //         thumbnail: "product6.jpeg",
+    //         price: 67900
+    //     }
+    // ];
 
-    // $() => querySelector, querySelectorAll 에 해당
-    $("#bestProducts").empty();
+    // // $() => querySelector, querySelectorAll 에 해당
+    // $("#bestProducts").empty();
 
-    products.forEach(function(product) {
-        let html = `<li>
-            <div class="box-image">
-                <img src="img/${product.thumbnail}"/>
-            </div>
-            <div class="info">
-                <div class="name">${product.name}</div>
-                <div class="price">
-                    <span class="value">${product.price.toLocaleString()}</span>원
-                </div>
-            </div>
-        </li>`;
+    // products.forEach(function(product) {
+    //     let html = `<li>
+    //         <div class="box-image">
+    //             <img src="img/${product.thumbnail}"/>
+    //         </div>
+    //         <div class="info">
+    //             <div class="name">${product.name}</div>
+    //             <div class="price">
+    //                 <span class="value">${product.price.toLocaleString()}</span>원
+    //             </div>
+    //         </div>
+    //     </li>`;
 
-        $("#bestProducts").append($(html));
-    });
+    //     $("#bestProducts").append($(html));
+    // });
 });
--- main_copy.js
+++ main_copy.js
@@ -3,20 +3,20 @@ const http = require('http');
 const fs = require('fs');
 const mimeTypes = require('mime-types');
 const mysql = require('mysql');
-const port=3000;
+const port=8000;
 
 const conn = mysql.createConnection({
     host:"localhost",
     port:"3306",
     user:"root",
-    password:"root1234",
+    password:"mirim2",
     database:"gmarket"
 })
 
 conn.connect();
 
 const sql={
-    list:'select * from products order by seq desc',
+    list:'select * from products',
     insert:'insert into products(name, price, img, click)value(?,?,?,?) ',
     read:'select * from products where id = ?'
     // update : 'update emp set name=?, emp_number=?, email=? where id=?',
@@ -32,31 +32,53 @@ const server = http.createServer((request, response) => {
             path = "./main_copy.html";
             break;
         case "/api/proudcts/best":
-            console.log("api/products/best");
-            // conn.query(sql.list,(err,data)=>{
-            //     response.writeHead(200);
-            //     response.end(data);
-            // });
-            response.writeHead(200);
-            response.end("<h1>Hello World</h1>");
+            conn.query(sql.list,(err,data)=>{
+                if(err) console.log(err);
+                else{
+                    for(var i=0;i<data.lenght;i++){
+                        response.writeHead(200,{'Content-Type':'text/html; charset=utf-8'}); 
+                        response.write(`<li>
+                                 <div class="box-image">
+                                    <img src="img/${data[i].image}"/>
+                                 </div>
+                                 <div class="info">
+                                     <div class="name">${data[i].name}</div>
+                                     <div class="price">
+                                         <span class="value">${data[i].price.toLocaleString()}</span>원
+                                     </div>
+                                 </div>
+                             </li>`);
+                        
+                    }
+                    //data.forEach(function(product) {
+                        //response.write(html);
+                               // ,{'Content-Type':'text/html'}
+                         //response.write(`<h2>${data[0].name}Hello world</h2>`);
+                    // })  // foreach 끝   
+                    
+                }   // else 끝
+                response.end(); 
+            });
+            
             break;
         default:
             path = `./${request.url.substr(1)}`;
             break;
     }
-
-    fs.readFile(path, (error, data) => {
-        if(error) {
-            response.writeHead(404);
-            response.end();
-            return;
-        }
-
-        response.writeHead(200, {
-            "Content-Type": mimeTypes.lookup(path)
+    if(path !== undefined){
+        fs.readFile(path,(error, data) => {
+            if(error) {
+                response.writeHead(404);
+                response.end();
+                return;
+            }
+    
+            response.writeHead(200, {
+                "Content-Type": mimeTypes.lookup(path)
+            });
+            response.end(data);
         });
-        response.end(data);
-    });
+    }
 });
 
 server.listen(port,()=>{
